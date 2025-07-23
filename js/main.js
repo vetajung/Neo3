7934c8c6c0e05ead6fbbc66a99e2e5bb27d0eca7
vetajung <30714656+vetajung@users.noreply.github.com>
2025-07-23 14:33:33 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,52 +3,35 @@ let storyStepIndex = 0;
 
 // 스토리 단계 정의
 const storySteps = [
-  { text: "오늘은 ${playerName}님의 첫 출근날입니다. 회사까지는 자율출퇴근제를 시행하고 있어 자유롭게 출근이 가능하다.", type: "text" },
-  { text: "이런 기회 흔치않다!", type: "text" },
+  { text: "${playerName} 은 파트장님에게 첫 업무를 배정받았다.", type: "text" },
+  { text: "내용은 거래처에 지난 회의자료를 전달하고, 일정 관련 확인 요청을 하는 간단한 업무.", type: "text" },
+  { text: "파트장님은 편한 방식으로 해봐요 라고만 했다", type: "text" },
   {
     type: "choice",
-    question: "언제까지 가는게 좋을까.. 마주치는 부서분들에게는 인사를 꼭 해야겠어!",
+    question: "어떤 방식이 좋을까?",
     options: [
       {
-        label: "8시 정각즈음 출근한다",
-        text: "성실하시군요!",
-        nextStep: 3
-      },
-      {
-        label: " 9시 30분에 출근한다",
-        text: "부서의 출근시간이 있는지 확인한 후 출근하면 더 베스트!",
+        label: "전화로 바로 연락한다",
+        text: "짧고 명확하게 스크립트를 간단히 사전에 메모하고,
+상대방이 통화 가능한지 먼저 확인해보자!",
         nextStep: 4
       },
       {
-        label: "회사 분위기/문화",
-        text: "천재군요",
+        label: "메일을 작성한다",
+        text: "동명이인에게 발송되는 실수가 일어나지 않도록 수신처를 확인하자
+수신자들이 이해할 수 있는지 맞춤법 및 내용을 재확인하자 ",
         nextStep: 5
+      },
+      {
+        label: "선배에게 먼저 물어본다",
+        text: "섣불리 확인 받기보단 초안을 먼저 써보고 선배에게 피드백을 구해보자
+피드백을 요청할떄는 공손하고 당당하게 요청하자",
+        nextStep: 6
       }
     ]
   },
   { text: "내일 궁금했던걸 물어볼 기회다.", type: "text" },
   { text: "이런 기회 흔치않다!!", type: "text" },
-  {
-    type: "choice",
-    question: "뭐 먹을까?",
-    options: [
-      {
-        label: "김치찌개",
-        text: "김치!",
-        nextStep: 6
-      },
-      {
-        label: "된장",
-        text: "된!",
-        nextStep: 6
-      },
-      {
-        label: "고추장",
-        text: "도추장!",
-        nextStep: 6
-      }
-    ]
-  },
   { text: "점심시간 끝! 다시 업무 시작!", type: "text" },
 ];
 
